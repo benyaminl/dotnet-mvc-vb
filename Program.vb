6dc9daabb836bb42abcc1a7b23c2132a17f2da5a
Benyamin Limanto <blbenyamin9@gmail.com>
2022-03-21 09:01:02 +0700

    cshtml works on .NET Core 6.0 VB.NET by Adding RazorPage and runtimecompilation

--- Program.vb
+++ Program.vb
@@ -6,6 +6,7 @@ Imports Microsoft.AspNetCore.Builder ' WebAppBuilder Need
 Imports Microsoft.AspNetCore.Hosting ' Set Content Root Folder
 Imports Microsoft.Extensions.DependencyInjection ' For builder.Service Need
 Imports Microsoft.Extensions.Hosting ' Check if Dev or Prod or Etc
+Imports Microsoft.AspNetCore.Razor
 
 Module Program
     Sub Main(args As String())
@@ -30,6 +31,13 @@ Module Program
         Dim builder as WebApplicationBuilder = WebApplication.CreateBuilder(args)
         builder.WebHost.UseContentRoot(Directory.GetCurrentDirectory())
         builder.Services.AddControllersWithViews()
+        ' @see https://stackoverflow.com/a/65283057/4906348
+        ' This fix the error of not found cshtml when debug/prod
+        Dim razorBuilder As IMvcBuilder = builder.Services.AddRazorPages() ' CSHTML?
+        If builder.Environment.IsDevelopment() Then
+            ' Need https://www.nuget.org/packages/Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation/
+            razorBuilder.AddRazorRuntimeCompilation()
+        End If
         Return builder.build()
     End Function
 End Module
